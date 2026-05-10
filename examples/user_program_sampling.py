0d8d710900880789d8522f717be26a31dfa08d00
11302 <uchida@sec.co.jp>
2026-05-10 23:57:27 +0900

    feat:Update examples to follow SSE feature expansion

--- examples/user_program_sampling.py
+++ examples/user_program_sampling.py
@@ -12,11 +12,15 @@ for index in range(3):
         circuit = QuantumCircuit(2)
         circuit.add_H_gate(0)
         circuit.add_CNOT_gate(0, 1)
+        mitigation_info = {
+            "ro_error_mitigation": "pseudo_inverse",
+        }
 
         job = OqtopusSamplingBackend().sample(
             circuit,
             shots=1000,
             device_id="sse",
+            mitigation_info=mitigation_info,
         )
         print(f"{job.job_id=}")
         result = job.result()
