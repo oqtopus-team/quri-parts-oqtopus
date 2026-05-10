a8c2bcf7584fc407f2c3e176e449d894683276de
11302 <uchida@sec.co.jp>
2026-05-10 23:57:27 +0900

    feat:Update examples to follow SSE feature expansion

--- examples/submit_job_sse_estimation.py
+++ examples/submit_job_sse_estimation.py
@@ -3,13 +3,14 @@ from quri_parts_oqtopus.backend import OqtopusConfig, OqtopusSseBackend
 backend = OqtopusSseBackend(OqtopusConfig.from_file("oqtopus-dev"))
 
 job = backend.run_sse(
-    file_path="examples/user_program.py",
+    file_path="examples/user_program_estimation.py",
     device_id="Kawasaki",
-    name="sse example",
+    name="sse estimation example",
 )
 print(job)
-counts = job.result().counts
-print(counts)
+result = job.result()
+print(result.exp_value)
+print(result.stds)
 
 job.download_log(
     save_dir="examples",
